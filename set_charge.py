0ae3b9d957fa2a7db51be409b3c95278770d3795
lawrencegripper <info@grippers.co.uk>
2023-04-06 21:10:26 +0100

    Add backoff. Don't ship cache forcast

--- set_charge.py
+++ set_charge.py
@@ -5,6 +5,8 @@
 # Solcast give limited free API calls, so cache the data for testing
 import datetime
 import os
+import sys
+import backoff
 from pysolcast.rooftop import RooftopSite
 import json
 from givenergy_modbus.client import GivEnergyClient
@@ -12,6 +14,39 @@ from givenergy_modbus.model.inverter import Model
 from givenergy_modbus.model.plant import Plant
 import requests
 
+def send_signal(msg):
+    signal_request = {
+        "number": os.environ["SIGNAL_SEND"],
+        "recipients": [ os.environ["SIGNAL_RECEIVE"] ],
+        "message": msg,
+    }
+    requests.post("http://ubuntu-utilities:30027", json = signal_request)
+
+
+def on_backoff(details):
+    print(f"Backing off {details['wait']} seconds afters {details['tries']} tries calling function {details['target']}. Args: {details['args']}, kwargs: {details['kwargs']}")
+    type, value, traceback = sys.exc_info()
+    print(value)
+    print(traceback)
+    send_signal(f"Backing off {details['wait']} seconds afters {details['tries']} tries calling function. Error: {value}")
+
+@backoff.on_exception(backoff.expo, Exception, max_tries=5, on_backoff=on_backoff)
+def update_inverter(predicted_charge_percentage):
+    # Connect to inverter
+    client = GivEnergyClient(host=os.environ["INVERTOR_IP_1"])
+
+    # Set data
+    client.set_battery_target_soc(predicted_charge_percentage)
+    client.set_charge_slot_1((datetime.time(hour=0, minute=30), datetime.time(hour=4, minute=00)))
+    # # set the inverter to charge when there's excess, and discharge otherwise. it will also respect charging slots.
+    # client.set_mode_dynamic()
+
+    # Check data is written
+    p = Plant(number_batteries=1)
+    client.refresh_plant(p, full_refresh=True)
+    send_signal(f"Confirmed overnight charge set to {p.inverter.charge_target_soc}%")
+
+    print(p.inverter.charge_target_soc)
 
 cache_file = 'forcast-cache.json'
 if os.path.exists(cache_file):
@@ -67,25 +102,7 @@ print(f"Predicted charge percentage: {predicted_charge_percentage}")
 if predicted_charge_percentage > 100:
     predicted_charge_percentage = 100
 
-signal_request = {
-    "number": os.environ["SIGNAL_SEND"],
-    "recipients": [ os.environ["SIGNAL_RECEIVE"] ],
-    "message": f"Set overnight charge percentage to {predicted_charge_percentage}%\n\nPredicted generation for tomorrow: {predicted_generation}kwh\nTotal estimated usage: {total_usage_kw}kwh",
-}
-requests.post("http://ubuntu-utilities:30027", json = signal_request)
+msg = f"Set overnight charge percentage to {predicted_charge_percentage}%\n\nPredicted generation for tomorrow: {predicted_generation}kwh\nTotal estimated usage: {total_usage_kw}kwh"
+send_signal(msg)
 
-client = GivEnergyClient(host=os.environ["INVERTOR_IP_1"])
-
-# Set data
-client.set_battery_target_soc(predicted_charge_percentage)
-client.set_charge_slot_1((datetime.time(hour=0, minute=30), datetime.time(hour=4, minute=00)))
-# # set the inverter to charge when there's excess, and discharge otherwise. it will also respect charging slots.
-# client.set_mode_dynamic()
-
-# Check data is written
-p = Plant(number_batteries=1)
-client.refresh_plant(p, full_refresh=True)
-signal_request["message"] = f"Confirmed overnight charge set to {p.inverter.charge_target_soc}%"
-requests.post("http://ubuntu-utilities:30027", json = signal_request)
-
-print(p.inverter.charge_target_soc)
+update_inverter(predicted_charge_percentage)
